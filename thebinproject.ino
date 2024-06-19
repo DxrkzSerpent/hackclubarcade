70e34d171025c1484b818170d832eec834077a8f
Eric Zheng <93160458+DxrkzSerpent@users.noreply.github.com>
2024-06-18 21:49:09 -0400

    Update thebinproject.ino

--- thebinproject.ino
+++ thebinproject.ino
@@ -12,7 +12,7 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
   { '*', '0', '#', }
 };
 
-#define NEOPIXEL 17       
+#define NEOPIXEL 17
 #define NUMPIXELS 1
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
@@ -20,28 +20,50 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, NEOPIXEL, NEO_GRB + NEO_K
 
 #define motion 16
 
+const String password = "6089"; // Change this to your desired password
+String inputPassword;
+
 void setup() {
   pinMode(motion, INPUT);
   strip.begin();
   strip.show();
   Serial1.begin(115200);
+  keypad.addEventListener(keypadEvent);
 }
 
 
 void loop() {
   bool motion_detected = digitalRead(motion);
-  char key = keypad.getKey();
-
-  if (motion_detected) {
+  if (digitalRead(motion) == HIGH) {
+    Serial1.println("Motion detected! Enter password:");
+    char key = keypad.getKey();
+    if (key) {
+      keypadEvent(key);
+    }
     strip.setPixelColor(0, strip.Color(255, 255, 255));
     strip.show();
-    if (key != NO_KEY) {
-      Serial.println(key);
-    }
   } else {
-    strip.setPixelColor(0,strip.Color(0, 0, 0));
+    strip.setPixelColor(0, strip.Color(0, 0, 0));
     strip.show();
   }
 
-  delay(1); 
+  delay(1);
+}
+
+void keypadEvent(KeypadEvent key) {
+  if (key == '*') {
+    inputPassword = ""; // Reset the input password
+  } else if (key == '#') {
+    // Check if the input password matches the set password
+    if (inputPassword == password) {
+      Serial.println("Access Granted!");
+      strip.setPixelColor(0, strip.Color(0, 255, 0)); // Green
+      strip.show();
+    } else {
+      Serial.println("Access Denied!");
+    }
+    inputPassword = ""; // Reset the input password after checking
+  } else {
+    inputPassword += key; // Append the pressed key to the input password
+  }
 }
