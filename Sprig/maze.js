7ea006e4d628cdd4067015332b7b9f274935dc4e
Eric Zheng <93160458+DxrkzSerpent@users.noreply.github.com>
2024-06-20 20:03:24 -0400

    Update maze.js

--- Sprig/maze.js
+++ Sprig/maze.js
@@ -1,193 +1,141 @@
 /*
-@title: getting_started
-@tags: ['beginner', 'tutorial']
-@addedOn: 2022-07-26
-@author: leo, edits: samliu, belle, kara
+First time? Check out the tutorial game:
+https://sprig.hackclub.com/gallery/getting_started
 
-Check the tutorial in the bottom right, the run button is in the top right.
-Make sure to remix this tutorial if you want to save your progress!
+@title: 
+@author: 
+@tags: []
+@addedOn: 2024-00-00
 */
 
-// define the sprites in our game
-const player = "p";
-const box = "b";
-const goal = "g";
-const wall = "w";
-const myTune = tune`
-500: F5~500,
-15500`;
+const player = "p"
+const sky = "s"
+const border = "b"
+const end = "e"
 
-// assign bitmap art to each sprite
+let level = 0
+const levels = [
+  map`
+bbbbbbbbbb
+bpb......b
+b.b.bbbb.b
+b.b.b....b
+b.b.b.bb.b
+b.b.b.bb.b
+b.b.b.eb.b
+b.b.bbbb.b
+b.....bb.b
+bbbbbbbbbb`,
+  map`
+bbbbbbbbbb
+bp.......b
+bbbbb.bb.b
+bbbbb..b.b
+bb..eb.b.b
+bb.bbb.b.b
+bb.....b.b
+b..bbbbb.b
+b.bb.....b
+bbbbbbbbbb`
+];
+
 setLegend(
   [player, bitmap`
 ................
-................
-................
-.......0........
-.....00.000.....
-....0.....00....
-....0.0.0..0....
-....0......0....
-....0......0....
-....00....0.....
 ......00000.....
-......0...0.....
-....000...000...
+.....00...0.....
+....00....00....
+....0......0....
+....00...000....
+.....00..0......
+......0000......
+.......00.......
+....00000000....
+.......00.......
+.......00.......
+......0000......
+.....00..00.....
+.....0....0.....
+.....0....0.....`],
+  [sky, bitmap`
+7777777777777777
+7722777777777772
+7222277777777722
+7777777722777777
+7777777777777777
+7777777777777777
+7777777777722777
+7777777777222277
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777`],
+  [border, bitmap`
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL
+LLLLLLLLLLLLLLLL`],
+  [end, bitmap`
+................
+................
+................
+................
+....HHHHHHHH....
+....H555555H....
+....H5HHHH5H....
+....H5H55H5H....
+....H5H55H5H....
+....H5HHHH5H....
+....H555555H....
+....HHHHHHHH....
+................
 ................
 ................
 ................`],
-  [box, bitmap`
-................
-................
-................
-...88888888888..
-...8....8....8..
-...8....8....8..
-...8....8....8..
-...8....8....8..
-...88888888888..
-...8....8....8..
-...8....8....8..
-...8....8....8..
-...8....8....8..
-...88888888888..
-................
-................`],
-  [goal, bitmap`
-................
-................
-................
-....444444......
-...44....44.....
-...4......4.....
-...4.......4....
-...4.......4....
-...4.......4....
-...44......4....
-....4......4....
-....44....44....
-.....444444.....
-................
-................
-................`],
-  [wall, bitmap`
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000
-0000000000000000`]
 );
 
-// create game levels
-let level = 0; // this tracks the level we are on
-const levels = [
-  map`
-..p.
-.b.g
-....`,
-  map`
-p..
-.b.
-..g`,
-  map`
-p.wg
-.b..
-....
-..w.`,
-  map`
-p...
-...b
-...b
-.bbg`,
-  map`
-...
-.p.
-...`,
-  map`
-p.w.
-.bwg
-....
-..bg`
-];
+setSolids([player, border])
+setMap(levels[level])
 
-// set the map displayed to the current level
-const currentLevel = levels[level];
-setMap(currentLevel);
-
-setSolids([player, box, wall]); // other sprites cannot go inside of these sprites
-
-// allow certain sprites to push certain other sprites
 setPushables({
-  [player]: [box],
-  [box]: [box]
-});
+  [player]: []
+})
 
-// inputs for player movement control
+
 onInput("w", () => {
-  getFirst(player).y -= 1; // positive y is downwards
+  getFirst(player).y -= 1;
 });
-
-onInput("s", () => {
-  getFirst(player).y += 1; // positive y is downwards
-});
-
 onInput("a", () => {
   getFirst(player).x -= 1;
 });
-
+onInput("s", () => {
+  getFirst(player).y += 1;
+});
 onInput("d", () => {
   getFirst(player).x += 1;
 });
 
-// input to reset level
-onInput("j", () => {
-  playTune(myTune);
 
-  const currentLevel = levels[level]; // get the original map of the level
-
-  // make sure the level exists before we load it
-  if (currentLevel !== undefined) {
-    clearText("");
-    setMap(currentLevel);
-  }
-});
-
-// these get run after every input
 afterInput(() => {
-  playTune(myTune);
-
-  // count the number of tiles with goals
-  const targetNumber = tilesWith(goal).length;
-
-  // count the number of tiles with goals and boxes
-  const numberCovered = tilesWith(goal, box).length;
-
-  // if the number of goals is the same as the number of goals covered
-  // all goals are covered and we can go to the next level
-  if (numberCovered === targetNumber) {
-    // increase the current level number
+  if (tilesWith(end, player).length == 1 && level < 5) { 
     level = level + 1;
-
-    const currentLevel = levels[level];
-
-    // make sure the level exists and if so set the map
-    // otherwise, we have finished the last level, there is no level
-    // after the last level
-    if (currentLevel !== undefined) {
-      setMap(currentLevel);
-    } else {
-      addText("you win!", { y: 4, color: color`3` });
-    }
+  } else if (level == 5) {
+    addText("you win!", {y: 4, color: color`5` });
   }
-});
+})
